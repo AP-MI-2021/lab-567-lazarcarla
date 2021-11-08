1616e1198e099f979d7f7d783c3e1af412ad475b
lazarcarla <carlalazar123@yahoo.com>
2021-11-08 17:39:11 +0200

    carla lazar , problema 3, lab 7, problema finalizata

--- Tests/testUndoRedo.py
+++ Tests/testUndoRedo.py
@@ -3,7 +3,7 @@ from Logic.CRUD import adaugaRezervare, stergeRezervare, getById
 
 
 def testUndoRedo():
-    #1 adauga o rezervare
+
     lista = []
     undoList = []
     redoList = []
@@ -19,33 +19,34 @@ def testUndoRedo():
     lista = adaugaRezervare("3", "Denisa", "economy plus", 50.5, "nu", lista)
     undoList.append(lista)
     lista=rezultat
-    #5
+    #5 undo scote ultina rezervare
     redoList.append(lista)
     lista=undoList.pop()
     assert len(lista) == 2
     assert getId(lista[1]) == "2"
     assert getId(lista[0]) == "1"
-    #6
+    #6 undo scoate penultima rezervare
     redoList.append(lista)
     lista = undoList.pop()
     assert len(lista) == 1
     assert getId(lista[0]) == "1"
     assert undoList == [[]]
-    #7
+    #7 undo scoate prima rezervare
     redoList.append(lista)
     lista = undoList.pop()
     assert len(lista) == 0
-    assert undoList == [[]]
-    #8
+    assert undoList == []
+    #8 undo care nu face nimic
     if len(undoList) > 0:
         redoList.append(lista)
         lista=undoList
     assert len(lista) == 0
-    assert undoList == [[]]
-    #9
+    assert undoList == []
+    #9 adaugam 3 rezervari
     rezultat = adaugaRezervare("1", "Mihai", "business", 4300.0, "nu", lista)
     undoList.append(lista)
     lista = rezultat
+    redoList.clear()
 
     rezultat = adaugaRezervare("2", "Denis", "economy plus", 300.0, "da", lista)
     undoList.append(lista)
@@ -57,50 +58,50 @@ def testUndoRedo():
     assert len(redoList) == 0
     assert len(UndoList) == 3
     assert len(lista) == 3
-    #10
+    #10 facem redo
     if len(redoList) > 0:
         undoList.append(lista)
         lista=redoList.pop()
     assert len(redoList) == 0
     assert len(undoList) == 3
     assert len(lista) == 3
-    #11
+    #11 2 undo
     redoList.append(lista)
-    lista=undoList.pop
+    lista=undoList.pop()
 
     assert len(lista) == 2
     assert getId(lista[1]) == "2"
     assert getId(lista[0]) == "1"
 
     redoList.append(lista)
-    lista = undoList.pop
+    lista = undoList.pop()
 
     assert len(lista) == 1
     assert getId(lista[0]) == "1"
     assert undoList == [[]]
 
-    #12
+    #12 redo
     undoList.append(lista)
     lista=redoList.pop()
     assert len(redoList) ==1
     assert len(undoList) == 2
     assert len(lista) == 2
 
-    #13
+    #13 redo
     undoList.append(lista)
     lista=redoList.pop()
     assert len(redoList) == 0
     assert len(undoList) == 3
     assert len(lista) == 3
 
-    #14
+    #14 se fac 2 undo
 
     redoList.append(lista)
     lista=undoList.pop()
     assert len(lista) == 2
     assert getId(lista[1]) =="2"
     assert getId(lista[0]) == "1"
-    assert undoList== [[]]
+
 
     redoList.append(lista)
     lista = undoList.pop()
@@ -108,13 +109,13 @@ def testUndoRedo():
     assert getId(lista[0]) == "1"
     assert undoList == [[]]
 
-    #15
+    #15 adaugam a 4 a rezervare
     rezultat = adaugaRezervare("4", "Cristian", "business", 1000.0, "da", lista)
     undoList.append(lista)
     lista = rezultat
     redoList.clear()
 
-    #16
+    #16 redo
 
     if len(redoList) > 0:
         undoList.append(lista)
@@ -122,7 +123,7 @@ def testUndoRedo():
     assert len(lista)==2
     assert len(undoList)==2
 
-    #17
+    #17 se face undo
 
     redoList.append(lista)
     lista = undoList.pop()
@@ -130,7 +131,7 @@ def testUndoRedo():
     assert len(redoList) == 1
     assert len(undoList) == 1
 
-    #18
+    #18 undo
 
     redoList.append(lista)
     lista = undoList.pop()
@@ -138,7 +139,7 @@ def testUndoRedo():
     assert len(redoList) == 2
     assert len(undoList) == 0
 
-    #19
+    #19 2 redo
 
     undoList.append(lista)
     lista = redoList.pop()
@@ -149,7 +150,7 @@ def testUndoRedo():
     assert len(lista) == 2
     assert len(redoList) == 0
 
-    #20
+    #20 ultimul redo care nu face nimic
 
     if len(redoList) >0:
         undoList.append(lista)
